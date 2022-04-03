3a8fc0c41f7f1e28d4ef43847333f49a4c51c76e
Subin Siby <mail@subinsb.com>
2022-04-03 16:19:03 +0530

    Add inital version

---
+++ src/App.vue
@@ -0,0 +1,37 @@
+<script setup>
+import { RouterLink, RouterView } from "vue-router";
+</script>
+
+<template>
+  <RouterView />
+</template>
+
+<style>
+@import "@/assets/base.css";
+
+#app {
+  max-width: 1280px;
+  margin: 0 auto;
+  padding: 1rem;
+
+  font-weight: normal;
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+a,
+.green {
+  text-decoration: none;
+  color: hsla(160, 100%, 37%, 1);
+  transition: 0.4s;
+}
+
+@media (hover: hover) {
+  a:hover {
+    background-color: hsla(160, 100%, 37%, 0.2);
+  }
+}
+</style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,36 @@
+<script setup>
+import * as punycode from "punycode";
+
+let host = window.location.host;
+if (host.indexOf("xn--") === -1) {
+  // For localhost, use sample "നോക്കി-ഇരുന്നോ-ഇപ്പോ.കിട്ടും.com"
+  host = "xn-----63hb0ea5keawa0ewk5a7ljewhf.xn--rvc1b4aa2noa6f.com";
+}
+const hostInUnicode = punycode.toUnicode(host);
+
+let siteName = () => {
+  const withoutCom = hostInUnicode.slice(0, -4);
+  return withoutCom.replace(/[-.]/g, " ");
+};
+</script>
+
+<template>
+  <main>
+    <h1 class="title">{{ siteName() }}</h1>
+    <h2>😌😌😌</h2>
+  </main>
+</template>
+
+<style scoped>
+main {
+  text-align: center;
+}
+
+.title {
+  font-size: 3.5rem;
+}
+
+p {
+  margin: 1rem;
+}
+</style>
