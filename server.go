da09dbb3ff7737a3a8f8c8aa7ddac6ced3d3143a
Subin Siby <mail@subinsb.com>
2022-07-16 17:33:13 +0530

    go-native page.json parse

--- server.go
+++ server.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"io"
 	"net/http"
 	"text/template"
@@ -12,13 +13,34 @@ type TemplateRegistry struct {
 	templates *template.Template
 }
 
+type PageInfo struct {
+	Regex       string
+	Title       string
+	Subtitle    string
+	Description string
+}
+
+var pages []PageInfo
+
+func loadPagesJSON() {
+	data, err := ioutil.ReadFile("")
+	if err != nil {
+		log.Panicf("failed reading data from file: %s", err)
+	}
+	json.Unmarshal(, &pages)
+}
+
+func getTitle() {
+
+}
+
 func (t *TemplateRegistry) Render(w io.Writer, name string, data interface{}, c echo.Context) error {
 	return t.templates.ExecuteTemplate(w, name, data)
 }
 
 func Index(c echo.Context) error {
 	return c.Render(http.StatusOK, "index.html", map[string]interface{}{
-		"title": "HOME",
+		"title": getTitle(),
 	})
 }
 
